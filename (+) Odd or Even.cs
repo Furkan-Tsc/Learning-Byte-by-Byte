753489340a792399b8f2a6be8d01f7874e2874c9
Furkan Taşcı <furki38600@outlook.com>
2026-01-27 01:58:28 +0300

    Update (+) Odd or Even.cs

--- (+) Odd or Even.cs
+++ (+) Odd or Even.cs
@@ -1,35 +1,33 @@
 using System;
 
-namespace Odd_or_Even
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int sayi;
+
+        Console.Write("Sayı Giriniz: ");
+        
+        try
         {
-            int sayi;
-
-            Console.Write("Sayı Giriniz: ");
-
-            try
-            {
-                sayi = int.Parse(Console.ReadLine());
-            }
-            catch (FormatException) //input olarak sayı yerine harf vs. girilirse programın çökmesini engelle
-            {
-                Console.WriteLine("Sayı girmediniz");
-                return;
-            }
-
-            if (sayi % 2 == 0) // sayinin 2ye bölümünden kalan 0 ise çift
-            {
-                Console.WriteLine("Sayı Çift");
-            }
-            else // değilse tek
-            {
-                Console.WriteLine("Sayı Tek");
-            }
-
-            Console.ReadKey();
+            sayi = int.Parse(Console.ReadLine());
         }
+        catch (FormatException) //input olarak sayı yerine harf vs. girilirse programın çökmesini engelle
+        {
+            Console.WriteLine("Sayı girmediniz");
+            return;
+        }
+
+        if (sayi % 2 == 0) // sayinin 2ye bölümünden kalan 0 ise çift
+        {
+            Console.WriteLine("Sayı Çift");
+        }
+        else // değilse tek
+        {
+            Console.WriteLine("Sayı Tek");
+        }
+
+        Console.ReadKey();
     }
 }
+
